f39953a2c85b05da4ff55f57f914ae5b39342dad
i62vehef <i62vehef@uco.es>
2018-10-29 17:37:58 +0100

    arreglado cliente

--- P2/cliente.cpp
+++ P2/cliente.cpp
@@ -58,96 +58,16 @@ int main(int argc, char **argv){
 
       send(sd, buffer, sizeof(buffer), 0);
       recv(sd, buffer1, sizeof(buffer1), 0);
-      if(strcmp(buffer1, "SALIR\0")==0){
+
+      if(strcmp(buffer1, "+OK Waiting for a player\0")==0)
+         recv(sd, buffer1, sizeof(buffer1), 0);
+      
+      if(strcmp(buffer1, "SALIR\0")==0)
          break;
-      }
+      
       else{
          std::cout << buffer1 << '\n';
       }
    }
-   // while(1){
-   //    std::cout << "Introduzca que quiere hacer" << '\n';
-   //    std::cout << "1. Logearse" << '\n';
-   //    std::cout << "2. Registrase" << '\n';
-   //    std::cin >> opcion;
-   //
-   //    if(opcion!=0){
-   //       switch (opcion) {
-   //          case 1:
-   //             std::cout << "Introduzca el usuario" << '\n';
-   //             std::cin>>buffer1;
-   //             strcat(a, buffer1);
-   //             std::cout << a << '\n';
-   //             // buffer1 = "USUARIO " + buffer1;
-   //             send(sd, a, sizeof(a), 0);
-   //             recv(sd, buffer, sizeof(buffer), 0);
-   //
-   //             if(buffer=="error"){
-   //                std::cout << "no existe" << '\n';
-   //                break;
-   //             }
-   //
-   //             strcpy(buffer1, "");
-   //             std::cout << "Introduzca passwd" << '\n';
-   //             std::cin >> buffer1;
-   //
-   //             strcat(b, buffer1);
-   //             std::cout << b << '\n';
-   //             strcpy(buffer, "");
-   //             send(sd, b, sizeof(b), 0);
-   //             recv(sd, buffer, sizeof(buffer), 0);
-   //             if(buffer=="error"){
-   //                std::cout << "contraseña equivocada" << '\n';
-   //                break;
-   //             }
-   //             break;
-   //          case 2:
-   //             // std::cout << "Introduzca el usuario" << '\n';
-   //             // std::cin>>usuario;
-   //             // std::cout << "Introduzca passwd" << '\n';
-   //             // std::cin >> passwd;
-   //             //
-   //             // registro="REGISTRO -u " + usuario + " -p " + passwd;
-   //             // send(sd, registro.c_str(), sizeof(registro), 0);
-   //             // cadena="";
-   //             // recv(sd, cadena.c_str(), sizeof(cadena), 0);
-   //             // if(cadena=="error"){
-   //             //    std::cout << "El usuario existe" << '\n';
-   //             // }
-   //             break;
-   //
-   //          default:
-   //             std::cout << "opcion equivocada" << '\n';
-   //          break;
-   //       }
-   //    }
-   //    else{
-   //       close(sd);
-   //       std::cout << "salir" << '\n';
-   //       return 0;
-   //    }
-   // }
 
-   // if(cadena=="OK"){
-   //    int opcion2=0;
-   //    std::cout << "Introduzca que quiere hacer" << '\n';
-   //    std::cout << "1. Iniciar partido" << '\n';
-   //    std::cout << "2. salir" << '\n';
-   //    std::cin >> opcion2;
-   //
-   //    if(opcion2==1){
-   //       // partida(sd);
-   //    }
-   //    else{
-   //       close(sd);
-   //       std::cout << "salir" << '\n';
-   //       return 0;
-   //    }
-   // }
 }
-// void partida(int const & sd){
-//    std::string mensaje="JUGAR", confirmacion;
-//    send(sd, mensaje.c_str(), sizeof(mensaje), 0);
-//    std::cout << "Espere....." << '\n';
-//    recv(sd, confirmacion.c_str(), sizeof(confirmacion), 0);
-// }
